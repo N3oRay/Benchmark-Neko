cce5b498e09cafb5b693911aaf458d6064b1a26d
N3oRay <serge.lopes.94@free.fr>
2025-05-22 12:35:33 +0200

    Update Main_test.hx

--- Source/Main_test.hx
+++ Source/Main_test.hx
@@ -3,7 +3,21 @@ import openfl.display.Sprite;
 class Main extends Sprite {
     public function new() {
         super();
-        var menu = new MenuContext(["Start Game", "Options", "Exit"]);
+        
+        var menu = new MenuContext([
+            { label: "Play", action: function() {
+                trace("Play option selected!");
+                // Add your game start logic here
+            }},
+            { label: "Settings", action: function() {
+                trace("Settings option selected!");
+            }},
+            { label: "Quit", action: function() {
+                trace("Quit option selected!");
+            }}
+        ]);
         addChild(menu);
+
+        
     }
 }
